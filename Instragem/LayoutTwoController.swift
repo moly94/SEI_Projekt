377a3c35f3169a7421c317f724b66d3442691356
Franziska Desch <franziskadesch@franziskas-air.home>
2017-03-04 16:40:56 +0100

    gzjgjg

--- Instragem/LayoutTwoController.swift
+++ Instragem/LayoutTwoController.swift
@@ -9,4 +9,39 @@
 import UIKit
 
 class LayoutTwoController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextViewDelegate{
+    
+    
+    @IBOutlet weak var addButton1: UIButton!
+    
+    @IBOutlet weak var addButton2: UIButton!
+    
+    @IBOutlet weak var addButton3: UIButton!
+    
+   
+    @IBOutlet weak var addButton4: UIButton!
+    
+    @IBOutlet weak var addButton5: UIButton!
+
+    @IBOutlet weak var pickedImage1: UIImageView!
+    
+    @IBOutlet weak var pickedImage2: UIImageView!
+    
+    @IBOutlet weak var pickedImage3: UIImageView!
+    
+    
+    @IBOutlet weak var pickedImage4: UIImageView!
+    
+    @IBOutlet weak var pickedImage5: UIImageView!
+    
+    
+    
+    @IBOutlet weak var textView1: UITextView!
+    
+    
+    @IBOutlet weak var textView2: UITextView!
+    
+    
+    @IBOutlet weak var textView3: UITextView!
+    
+    @IBOutlet weak var textView4: UITextView!
 }
